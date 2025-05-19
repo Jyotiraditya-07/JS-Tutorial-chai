dde59fce3517864b05f6a9b153a914d6701b0f2f
Jyotiraditya Das <159943252+Jyotiraditya-07@users.noreply.github.com>
2025-05-19 20:14:46 +0000

    day 02

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,17 @@
-let score = "44"
+// let score = "44"
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33abs" => NaN
 // "33"=> 33
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log("1" +2 +2)
+console.log(" ")
